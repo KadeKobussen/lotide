6b30d80505d15c8ceb34a66f1618b4155abc96f4
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-06 12:49:14 -0800

    created an assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("💚💚💚Assertion Passed: " + actual + ' === ' + expected);
+  } else {
+    console.log('💔💔💔Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual("bootcamp", "bootcamp");
+assertEqual(1,9);
