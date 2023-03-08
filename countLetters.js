c7b0d28c870a9906a660df6119fd4888256cc535
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-08 15:07:43 -0800

    coded a function that counts how many times each letter is used and returns them as an object

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💔💔💔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(string){
+  const charMap = {};
+  for(let char of string){
+   if(char !== ' '){
+    if(charMap[char]){
+      charMap[char] = charMap[char] +1
+    } else {
+      charMap[char] = 1
+    }
+  }
+  }
+  return charMap;
+}
+console.log(countLetters('this was a little confusing to me'));
