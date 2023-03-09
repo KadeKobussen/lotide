dba38529de17381815779288792c7b893e0f0df5
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-08 17:05:38 -0800

    coded function letter positions thet tells you the index of each letter

--- letterPositions.js
+++ letterPositions.js
@@ -5,28 +5,28 @@ const eqArrays = function (arr1, arr2) {
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false; 
+      return false;
     }
   }
   return true;
 };
 
-const assertArraysEqual = function (arr1, arr2){
-  if(eqArrays(arr1, arr2) === true){
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
     console.log(`💚💚💚Assertion Passed: ${arr1} === ${arr2}`)
-  } else {console.log(`💔💔💔Assertion Failed: ${arr1} !== ${arr2}`)}
-  
+  } else { console.log(`💔💔💔Assertion Failed: ${arr1} !== ${arr2}`) }
+
 }
 
 
-const letterPositions = function(sentence){
+const letterPositions = function (sentence) {
   const charMap = {};
-  for(let i = 0; i < sentence.length; i++){
+  for (let i = 0; i < sentence.length; i++) {
     const char = sentence[i]
-   if(char !== ' '){
-    if(!charMap[char]) {
-      charMap[char] = [];
-    }
+    if (char !== ' ') {
+      if (!charMap[char]) {
+        charMap[char] = [];
+      }
       charMap[char].push(i)
     }
   }
