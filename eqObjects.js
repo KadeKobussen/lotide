e8d2fd8c8fcd2edd22b96bd6702266a59ebe3464
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-08 18:53:47 -0800

    made eqObjects function to check if objects are equal

---
+++ eqObjects.js
@@ -0,0 +1,65 @@
+const eqArrays = function (arr1, arr2) {
+  let result;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💔💔💔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqObjects = function (obj1, obj2) {
+  // checks if the length of the array of keys matches
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  // checks if they are arrays and if they are and they dont match it returns false
+  for (let key in obj1) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    } else {
+      // if not arrays checks if they are not equal and returns false if they are not equal
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+
+
+
+
+
+
+
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
+
+
+
+
+
+
+
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,16 @@
+const eqArrays = function (arr1, arr2) {
+  let result;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; 
+    }
+  }
+  return true;
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
